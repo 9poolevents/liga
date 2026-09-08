3766ac08fae00e56c072785cba4818f9be0f88e3
9poolevents <9poolevents@gmail.com>
2026-09-09 00:50:43 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// Salin file ini menjadi config.js, lalu isi URL Web App Google Apps Script.
 window.LIGA_CONFIG = {
   GOOGLE_SHEETS_BRIDGE_URL: "https://script.google.com/macros/s/AKfycbwsI23Nt1DM7ZN65mKhgDrIAiIhY0lKGoTbNZT4eghALzNHt9xWVM_YhL6E7fxwgc6n/exec"
 };
