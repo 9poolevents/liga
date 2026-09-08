3545526c0989e5d336a0506df32aa0d255664c3f
9poolevents <9poolevents@gmail.com>
2026-09-08 23:51:49 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Salin file ini menjadi config.js, lalu isi ID Google Sheet publik.
+// Versi ini menggunakan Google Visualization dan tidak membutuhkan API key.
+window.LIGA_CONFIG = {
+  GOOGLE_SHEET_ID: "1f4SOSBhHmlOPRqZ2fsRcpnXVEbeQNr2A"
+};
