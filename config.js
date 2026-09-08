73587505909df946b4988679e5bd4750080d69ff
9poolevents <9poolevents@gmail.com>
2026-09-08 23:06:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Jangan unggah config.js ke repository publik jika berisi API key.
 window.LIGA_CONFIG = {
   GOOGLE_SHEET_ID: "1f4SOSBhHmlOPRqZ2fsRcpnXVEbeQNr2A",
-  GOOGLE_SHEETS_API_KEY: "AIzaSyCmAyqN3p0ZycjK9t75bH5deVdphxYOxdQ"
+  GOOGLE_SHEETS_API_KEY: "AIzaSyDsVztUMN6ueln3vEggYn75hOyL1GpxOZ8"
 };
