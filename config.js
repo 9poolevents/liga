f327f992c24d26749015aad828e23d49e8020d38
9poolevents <9poolevents@gmail.com>
2026-09-08 23:49:58 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Salin file ini menjadi config.js, lalu isi nilainya.
-// Jangan unggah config.js ke repository publik jika berisi API key.
-window.LIGA_CONFIG = {
-  GOOGLE_SHEET_ID: "1f4SOSBhHmlOPRqZ2fsRcpnXVEbeQNr2A",
-  GOOGLE_SHEETS_API_KEY: "AIzaSyDsVztUMN6ueln3vEggYn75hOyL1GpxOZ8"
-};
